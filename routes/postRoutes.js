82c1d3fb8da9a60cb90cc495891b254ba8a30ddb
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-04-28 04:00:26 +0530

    not suing protectRoute

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -12,12 +12,12 @@ import protectRoute from "../middlewares/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/feed", protectRoute, getFeedPosts);
+router.get("/feed", getFeedPosts);
 router.get("/:id", getPost);
 router.get("/user/:username", getUserPosts);
-router.post("/create", protectRoute, createPost);
-router.delete("/:id", protectRoute, deletePost);
-router.put("/like/:id", protectRoute, likeUnlikePost);
-router.put("/reply/:id", protectRoute, replyToPost);
+router.post("/create", createPost);
+router.delete("/:id", deletePost);
+router.put("/like/:id", likeUnlikePost);
+router.put("/reply/:id", replyToPost);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,12 +14,12 @@ import protectRoute from "../middlewares/protectRoute.js";
 const router = express.Router();
 
 router.get("/profile/:query", getUserProfile);
-router.get("/suggested", protectRoute, getSuggestedUsers);
+router.get("/suggested", getSuggestedUsers);
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
-router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
-router.put("/update/:id", protectRoute, updateUser);
-router.put("/freeze", protectRoute, freezeAccount);
+router.post("/follow/:id", followUnFollowUser); // Toggle state(follow/unfollow)
+router.put("/update/:id", updateUser);
+router.put("/freeze", freezeAccount);
 
 export default router;
