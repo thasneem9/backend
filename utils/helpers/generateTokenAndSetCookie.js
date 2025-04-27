4b8d6e367c2e41eb8c1d74eaf7611b1cf10b1209
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2025-04-28 03:47:58 +0530

    lax

--- utils/helpers/generateTokenAndSetCookie.js
+++ utils/helpers/generateTokenAndSetCookie.js
@@ -8,7 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-		sameSite: "strict", // CSRF
+		sameSite: "lax", // CSRF
 	});
 
 	return token;
